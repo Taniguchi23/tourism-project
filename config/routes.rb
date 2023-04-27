301de6e7b3932b56b4cfe7974424cf23bae993b6
Sonny Kenyi Taniguchi Lock <sonny.taniguchi@utp-sistemas.com>
2023-04-27 11:49:21 -0500

    Carol-v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+ 
+  root  "home#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :admin do
+    #usuarios
+    get '/users', to: 'user#index', as: 'users'
+    get '/users/create', to: 'user#create', as: 'users_create'
+    post '/users/store', to: 'user#store', as: 'users_store'
+    get '/users/edit/:id', to: 'user#edit', as: 'users_edit'
+    post '/users/update/:id', to: 'user#update', as: 'users_update'
+    get '/users/delete/:id', to: 'user#delete', as: 'users_delete'
+  end
+  
 end
